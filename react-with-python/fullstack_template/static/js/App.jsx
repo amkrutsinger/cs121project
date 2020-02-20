f35cd1f329b75ee51a4af7c49fd9a159ea486bbb
s-c-t <sthomas@g.hmc.edu>
2020-02-19 16:11:30 -0800

    docs: add files from tutorial

---
+++ react-with-python/fullstack_template/static/js/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Hello from "./Hello";
+
+//require('../css/fullstack.css');
+var $ = require('jquery');
+
+import HeaderBackgroundImage from '../images/header.jpg';
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    addHeaderImg() {
+        let headerBg = new Image();
+        headerBg.src = HeaderBackgroundImage;
+    }
+
+    render() {
+        return (
+            <div className='header-contents'>
+                {this.addHeaderImg()}
+                <Hello name='Rimini' />
+            </div>
+        );
+    }
+}
