185735f48063be696e6053e9c0d04e8b1a9c8f9a
s-c-t <sthomas@g.hmc.edu>
2020-02-20 18:02:33 -0800

    refactor(entire project): change folder names

--- ourproject/app/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, url_for
-from app import app
-
-#url = url_for('static', filename='index.js')
-
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template("index.html")
