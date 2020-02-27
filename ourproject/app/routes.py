752b4569088b897f8e07b26073866c838653d2af
s-c-t <sthomas@g.hmc.edu>
2020-02-26 18:51:54 -0800

    style(python): reorder code to improve readibility

--- ourproject/app/routes.py
+++ ourproject/app/routes.py
@@ -1,7 +1,29 @@
 from flask import Flask, render_template, request
 from app import app
-import csv, io
-import requests
+import csv, io, requests, json
+
+# --- ROUTES --- #
+
+# Set up the homepage
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+# Parse user input and return result
+@app.route('/findRoutes', methods = ['POST'])
+def findRoutes():
+    if request.method == 'POST':
+        placesList = getInput()
+        distances, errors = parseInput(placesList)
+
+        # For Testing
+        print (', '.join(errors))
+    return render_template("index.html")
+
+
+
+# --- INTERFACE FUNCTIONS --- #
 
 # Input: .csv file (passed through request)
 # Output: list of items in .csv file
@@ -16,54 +38,78 @@ def getInput():
     placesList = [elt for lst in placesListOfLists for elt in lst]
     return placesList
 
+# Input: a list of places (as strings)
+# Output: the distance matrix for those places
+def parseInput(placesList):
+    coords, errors = addressesToCoordinates(placesList)
+    distances = distMatrix(coords)
+    return distances, errors
+
+# Input: a list of distances
+# Output: order to visit locations in in order to visit all locations
+#         in shortest amount of time
+def findShortestPath(distances):
+    return null
+
+
+
+# --- OPEN ROUTE SERVICE FUNCTIONS --- #
+
+    # Authorization key and other information to allow request for matrix data to process
+headers = {
+'Accept': 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
+'Authorization': '5b3ce3597851110001cf6248093639b34bd24267af2380b1d72d66d6',
+'Content-Type': 'application/json; charset=utf-8'
+}
+
+beginningOfUrl = 'https://api.openrouteservice.org/geocode/autocomplete?'
+api_key = '5b3ce3597851110001cf6248aa99e3ffa6984f3390e3f886fc85a33c'
+
+
 # return a matrix of distances given an input of coordinates
 # the output is a square matrix formatted as an array of a arrays
-def distMatrix(body):
-    # Authorization key and other information to allow request for matrix data to process
-    headers = {
-    'Accept': 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
-    'Authorization': '5b3ce3597851110001cf6248093639b34bd24267af2380b1d72d66d6',
-    'Content-Type': 'application/json; charset=utf-8'
-    }
+def distMatrix(listOfCoords):
+    body = {"locations": listOfCoords}
+    print(body)
+
     call = requests.post('https://api.openrouteservice.org/v2/matrix/driving-car', json=body, headers=headers)
+
     # Unwrap json to get a dictionary and access durations matrix
     return call.json()["durations"]
 
-# return routes
-def routes():
-    return "routes"
-    # if input != "":
-    #     return input + " route"
-    # else:
-    #     return ""
 
-# return map of area
-def mapArea():
-    return "map"
-    # if input != "":
-    #     return input + " map"
-    # else:
-    #     return ""
+# Input: a list of addresses
+# Output: a list of [longitude, latitude] coordinates corresponding to those addresses
+def addressesToCoordinates(list):
+    errors = []
+    coords = []
 
-# return errors
-def errors():
-    return "errors"
-    # if input != "":
-    #     return input + " errors"
-    # else:
-    #     return ""
+    for addr in list:
+        result = addressToCoord(addr)
+        if len(result) != 2:
+            errors.append(result)
+        else:
+            coords.append(result)
+    return coords, errors
 
-# Set up the homepage
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template("index.html", routes=routes(), map=mapArea(), errors=errors())
 
-# Parse user input and return result
-@app.route('/findRoutes', methods = ['POST'])
-def findRoutes():
-    if request.method == 'POST':
-        placesList = getInput()
-        body = {"locations":[[9.70093,48.477473],[9.207916,49.153868],[37.573242,55.801281],[115.663757,38.106467]]}
-        distMatrix(body)
-    return render_template("index.html", routes=routes(), map=mapArea(), errors=errors())
+# Input: an addresses
+# Output: the [longitude, latitude] returned by searching for that address on
+# OpenRouteService
+def addressToCoord(addr):
+    # this gets a JSON formatted list of locations matching input from OpenRouteService
+    call = requests.get(beginningOfUrl + 'api_key=' + api_key + '&text=' + addr, headers=headers)
+
+    # Error with OpenRouteService
+    if call.status_code != 200:
+        return "Error: " + call.reason
+    else:
+        # get list of all returned locations
+        call_dict = json.loads(call.text)
+        features = call_dict["features"]
+
+        if len(features) is 0:
+            return "Error: No result found for " + addr
+        else:
+            # return the coordinates of the top search result
+            return features[0]["geometry"]["coordinates"]
--- ourproject/react-frontend/src/App.js
+++ ourproject/react-frontend/src/App.js
@@ -3,25 +3,11 @@ import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            onChangeFile: null,
-            fileToBeSent: null
-        };
-        this.uploadFile = this.uploadFile.bind(this)
-    }
-
+    // Taken from Stack Overflow
     uploadFile(e) {
-      console.log(this.state.fileToBeSent)
-      console.log(this.state.onChangeFile)
-      console.log(this.state.file)
-      console.log(e.target.files[0])
-
       var config = { headers: {'Content-Type': undefined} };
 
       e.preventDefault();
-      //let file = this.state.fileToBeSent;
       let file = e.target.files[0];
       const formData = new FormData();
 
@@ -40,62 +26,17 @@ class App extends React.Component {
                 <div className="App-map">Map</div>
                 <div className="App-rightSide">
                   <div>Choose a CSV:</div>
-                    <input type="file" ref={(input) => { this.filesInput = input }} name="file" label="Upload CSV" onChange={this.uploadFile}/>
+                    <input type="file"
+                           ref={(input) => { this.filesInput = input }}
+                           name="file"
+                           label="Upload CSV" 
+                           onChange={this.uploadFile}
+                    />
                 </div>
               </header>
             </div>
         )
     }
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <div className="App-map">Map</div>
-//         <div className="App-rightSide">
-//           <div>Choose a CSV:</div>
-//           <form action="/findRoutes" method="post">
-//             <input type="file" name="file" onChange={this.onChangeFile}/>
-//                 <button onClick={this.uploadFile}>
-//                     Upload
-//                 </button>
-//              // <input type="file" id="placesCSV" name="placesCSV"></input>
-//              // <input type="submit"/>
-//           </form>
-//         </div>
-//       </header>
-//       {/*
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <p>
-//             Edit <code>src/App.js</code> and save to reload.
-//           </p>
-//           <p>My routes = {window.routes}</p>
-//           <p>My map = {window.map}</p>
-//           <p>My errors = {window.errors}</p>
-//           <a
-//             className="App-link"
-//             href="https://reactjs.org"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             Learn React
-//           </a>
-//         </header>
-//       </div>
-//
-//       <div class="form">
-//               <form action="/findRoutes" method="get">
-//                   <input type="text" name="place" />
-//                   <input type="submit" />
-//               </form>
-//       </div>
-//     */}
-//     </div>
-//   );
-// }
-
 
 export default App;
