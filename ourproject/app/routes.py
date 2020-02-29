c87527cf6c8d8290770d602b1a7daeadf665b1b5
s-c-t <sthomas@g.hmc.edu>
2020-02-29 14:45:17 -0800

    feat(shortest path): implement working algorithm with help of Google

--- ourproject/app/routes.py
+++ ourproject/app/routes.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request
 from app import app
-import csv, io, requests, json
+import csv, io, requests, json, sys
+
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
+
 
 # --- ROUTES --- #
 
@@ -11,17 +15,25 @@ import csv, io, requests, json
 def index():
     return render_template("index.html")
 
-# Parse user input and return result
+# Parse user input, generate distance matrix, and print routes
 @app.route('/findRoutes', methods = ['POST'])
 def findRoutes():
     if request.method == 'POST':
+        numPeople = 1
+
+        # Read in csv file and convert to array of places
         placesList = getInput()
+
+        # Convert array of places to distance matrix, array of invalid places
         distances, errors = parseInput(placesList)
 
-        # algorithm assumes starting at first location
-        # BUG: currently returns a path but not the shortest-path 
-        path = dijsktra(distances, placesList)
-        print(path)
+        # algorithm assumes starting and ending at first location
+        # routeTimes returned in seconds
+        # Find solution to Vehicle Routing Problem
+        maxRouteTime, actualRoutes, routeTimes = getOutput(distances, placesList, numPeople, sys.maxsize)
+        print(maxRouteTime)
+        print(actualRoutes)
+        print(routeTimes)
 
         # For Testing
         print (', '.join(errors))
@@ -51,51 +63,14 @@ def parseInput(placesList):
     distances = distMatrix(coords)
     return distances, errors
 
-# Input: a list of distances
-# Output: order to visit locations in in order to visit all locations
-#         in shortest amount of time
-# Method: Dijkstra's Algorithm
-# Source: Internet
-def dijsktra(distances, placesList):
-    # shortest paths is a dict of nodes
-    # whose value is a tuple of (previous node, weight)
-    initial = 0
-    shortest_paths = {initial: (None, 0)}
-    current_node = initial
-    visited = set()
-
-    unvisited = set()
-    for i in range(len(distances[0])):
-        unvisited.add(i)
-    unvisited.remove(0)
-
-    path = []
-
-    while len(unvisited) is not 0:
-        path.append(placesList[current_node])
-        visited.add(current_node)
-        destinations = unvisited
-        weight_to_current_node = shortest_paths[current_node][1]
-
-        for next_node in destinations:
-            weight = distances[current_node][next_node] + weight_to_current_node
-            if next_node not in shortest_paths:
-                shortest_paths[next_node] = (current_node, weight)
-            else:
-                current_shortest_weight = shortest_paths[next_node][1]
-                if current_shortest_weight > weight:
-                    shortest_paths[next_node] = (current_node, weight)
-
-        next_destinations = {node: shortest_paths[node] for node in shortest_paths if node not in visited}
-        if not next_destinations:
-            return "Route Not Possible"
-
-        # next node is the destination with the lowest weight
-        current_node = min(next_destinations, key=lambda k: next_destinations[k][1])
-        unvisited.remove(current_node)
-
-    return path
-
+# Input:  a list of distances and number of people
+# Output: a route for each person such that all locations are visited in the
+#         shortest amount of time
+def getOutput(distances, placesList, numPeople, vehicleMaxDistance):
+    data, manager, routing, solution = pathFinder(distances, numPeople, vehicleMaxDistance)
+    maxRouteTime, routes, routeTimes = analyze_solution(data, manager, routing, solution)
+    actualRoutes = [[placesList[location] for location in route] for route in routes]
+    return maxRouteTime, actualRoutes, routeTimes
 
 
 # --- OPEN ROUTE SERVICE FUNCTIONS --- #
@@ -157,3 +132,84 @@ def addressToCoord(addr):
         else:
             # return the coordinates of the top search result
             return features[0]["geometry"]["coordinates"]
+
+
+# --- SOLUTION TO VEHICLE ROUTING PROBLEM FROM GOOGLE --- #
+
+# Input:  array of distances between locations (by time)
+#         number of people to route
+# Output: a data model for the problem
+def create_data_model(distances, numPeople):
+    data = {}
+    data['distance_matrix'] = distances
+    data['num_vehicles'] = 1
+    data['depot'] = 0
+    return data
+
+
+# Given a solution, finds the individual routes
+def analyze_solution(data, manager, routing, solution):
+    max_route_distance = 0
+    routes = []
+    route_distances = []
+
+    for vehicle_id in range(data['num_vehicles']):
+        index = routing.Start(vehicle_id)
+        #plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
+        route_distance = 0
+        vehicleRoute = []
+
+        while not routing.IsEnd(index):
+            #plan_output += ' {} -> '.format(manager.IndexToNode(index))
+            vehicleRoute += [manager.IndexToNode(index)]
+            previous_index = index
+            index = solution.Value(routing.NextVar(index))
+            route_distance += routing.GetArcCostForVehicle(previous_index, index, vehicle_id)
+
+        vehicleRoute += [manager.IndexToNode(index)]
+
+        routes += [vehicleRoute]
+        route_distances += [route_distance]
+
+        # plan_output += 'Distance of the route: {}m\n'.format(route_distance)
+        # print(plan_output)
+        max_route_distance = max(route_distance, max_route_distance)
+    return max_route_distance, routes, route_distances
+
+
+# Input:  array of distances between locations (by time)
+#         number of people to route
+# Output: solution to Vehicle Routing Problem with given inputs
+def pathFinder(distances, numPeople, vehicleMaxDistance):
+    # Create the data problem, routing index manager, and routing model
+    data = create_data_model(distances, numPeople)
+    manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']), data['num_vehicles'], data['depot'])
+    routing = pywrapcp.RoutingModel(manager)
+
+    # Create and register a transit callback.
+    def distance_callback(from_index, to_index):
+        """Returns the distance between the two nodes."""
+        # Convert from routing variable Index to distance matrix NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return data['distance_matrix'][from_node][to_node]
+
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+    # Define cost of each arc.
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    routing.AddDimension( transit_callback_index, 0, vehicleMaxDistance,  True, 'Distance')
+    distance_dimension = routing.GetDimensionOrDie('Distance')
+    distance_dimension.SetGlobalSpanCostCoefficient(100)
+
+    # Setting first solution heuristic.
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+
+    # Solve the problem.
+    solution = routing.SolveWithParameters(search_parameters)
+
+    # Return solution
+    return data, manager, routing, solution
