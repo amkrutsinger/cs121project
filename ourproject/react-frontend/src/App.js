2429f3fcba0bd298085821854d390ff675adf1c5
s-c-t <sthomas@g.hmc.edu>
2020-03-06 13:31:17 -0800

    style(upload csv): add styling for upload csv page including button styling, consistent styling parameters between pages, and back button

--- ourproject/react-frontend/src/App.js
+++ ourproject/react-frontend/src/App.js
@@ -11,40 +11,54 @@ const mapStyles = {
 };
 
 
-// Display our Welcome - This includes
-//  - introductory text
-//  - button to get started (initiates steps)
+// Display our introductory text (w/ styling)
 function Welcome() {
     return (
-        <div className="about">
-           <p className ="welcome"> Welcome, </p>
-           <p className ="text"> We understand that planning routes for volunteers to canvas is a difficult and time-consuming task. As such, we have created this site to help with all your canvasing needs. Using state-of-the-art algorithms techniques, we can plan an efficient route for your volunteers to visit all canvassing locations in just a few short minutes. Happy campaigning! </p>
-           <button className="button">Get Started!</button>
+        <div className="description">
+           <p className="big-text"> Welcome, </p>
+           <p className="text"> We understand that planning routes for volunteers to canvas is a difficult and time-consuming task. As such, we have created this site to help with all your canvasing needs. Using state-of-the-art algorithms techniques, we can plan an efficient route for your volunteers to visit all canvassing locations in just a few short minutes. Happy campaigning! </p>
        </div>
     )
 }
 
 
-// Display Step 1 of the process - This includes
-//  - explanation of step 1
-//  - button to upload csv
+// Display instructions for step 1 of the process (uploading csv file)
 function Step1() {
     return (
-        <div className="step1">
-           <p className ="text"> Step 1: Upload a CSV file of the locations you want visited. </p>
-           <div>Choose a CSV:</div>
-           <input type="file"
-               ref={(input) => { this.filesInput = input }}
-               name="file"
-               label="Upload CSV"
-               onChange={this.uploadFile}
-           />
+        <div className="description">
+            <p className="big-text"> Step 1 </p>
+            <p className ="text"> Where do you want to campaign? </p>
        </div>
     )
 }
+
+// Display instructions for step 2 of the process (verifying addresses)
+function Step2() {
+    return (
+        <div className="description">
+            <p className="big-text"> Step 2 </p>
+            <p className ="text"> Here are the locations we got. Do you want to change anything? </p>
+       </div>
+    )
+}
+
 
 
 class App extends React.Component {
+
+    // Overview:
+    //    Welcome: introductory message, get started button
+    //    Step 1: Upload CSV file
+    //    Step 2: Add/Remove addresses
+    //    Step 3: Set Parameters (e.g. number of campaigners)
+    //    Result: display path, statistics
+
+    // Initialize states (what parts are visible)
+    state = {
+        isWelcomeActive: true,
+        isStep1Active: false,
+        isStep2Active: false,
+    }
 
     // Use: upload .csv file to flask/python for further analysis
     // Taken from Stack Overflow
@@ -61,7 +75,35 @@ class App extends React.Component {
         .catch(err => console.warn(err));
     }
 
-    // TODO: make background image only go to end of screen
+    // Show only Welcome component
+    showWelcome = () => {
+        this.setState({
+          isWelcomeActive: true,
+          isStep1Active: false,
+          isStep2Active: false
+        })
+    }
+
+    // Show only Step 1 component
+    showStep1 = () => {
+        this.setState({
+          isWelcomeActive: false,
+          isStep1Active: true,
+          isStep2Active: false
+        })
+    }
+
+    // Show only Step 2 component
+    showStep2 = () => {
+        this.setState({
+          isWelcomeActive: false,
+          isStep1Active: false,
+          isStep2Active: true
+        })
+    }
+
+
+
     render() {
         return (
             <div className="App">
@@ -69,40 +111,58 @@ class App extends React.Component {
                 <html>
                     <div className="page">
                         <div className="container">
-                            <Welcome />
+
+                            {this.state.isWelcomeActive &&
+                                <div className="welcome">
+                                    <Welcome />
+                                    <button className="button" onClick={this.showStep1}>Get Started!</button>
+                                </div>
+                            }
+
+                            {this.state.isStep1Active &&
+                                <div className="step1">
+                                    <button className="button-alt" onClick={this.showWelcome}>Back</button>
+                                    <Step1 />
+
+                                    {/* Input File Button */}
+                                    <input
+                                        type="file"
+                                        name="file"
+                                        id="file"
+                                        class="inputfile"
+                                        ref={(input) => { this.filesInput = input }}
+                                        onChange={e => { this.uploadFile(e); this.showStep2() }}
+                                    />
+                                    <label for="file">Choose a CSV file</label>
+                                </div>
+                            }
+
+                            {this.state.isStep2Active &&
+                                <div className="step2">
+                                    <button className="button-alt" onClick={this.showStep1}>Back</button>
+                                    <Step2 />
+
+                                    {/* Display Map  */}
+                                    {/* TODO: Show locations  */}
+                                    {/* TODO: Add Functionality to Add/Remove Addresses */}
+                                    <table className="App-header">
+                                        <tr className="App-row">
+                                            <th className="App-Sides">
+                                                <Map style={mapStyles} google={this.props.google} zoom={4} initialCenter={{ lat: 47.444, lng: -122.176}}></Map>
+                                            </th>
+                                            <th className="App-Sides">
+                                                <div className="text">Add Address</div>
+                                                <div className="text">Remove Address</div>
+                                            </th>
+                                        </tr>
+                                    </table>
+                                </div>
+                            }
+
                         </div>
-                    </div> 
+                    </div>
                 </html>
             </div>
-
-              	//  <div className="App-about">
-                //     Welcome.
-                //     <br/>
-                //     We understand that planning routes for volunteers to canvas is a difficult and time-consuming task. As such, we have created this site to help with all your canvasing needs. Using state-of-the-art algorithms techniques, we can plan an efficient route for your volunteers to visit all canvassing locations with just the click of a button.
-                //     <br/>
-                //     In order to get started, upload a csv file of the your canvassing locations. Our algorithm will do the rest. Once our algorithm has finished running, you can change the number of volunteers, view the generated routes, and get individual links to directions for each volunteer.
-                //     <br/>
-                //     Happy campaigning!
-                // </div>
-                //
-                // <table className="App-header">
-                //     <tr className="App-row">
-                //         <th className="App-Sides">
-                //             <Map style={mapStyles} google={this.props.google} zoom={4} initialCenter={{ lat: 47.444, lng: -122.176}}></Map>
-                //         </th>
-                //         <th className="App-Sides">
-                //             <div>Choose a CSV:</div>
-                //             <input type="file"
-                //                 ref={(input) => { this.filesInput = input }}
-                //                 name="file"
-                //                 label="Upload CSV"
-                //                 onChange={this.uploadFile}
-                //             />
-                //         </th>
-                //     </tr>
-                // </table>
-                //
-                // <div className="App-credits">This site uses OpenStreetRoute.</div>
         )
     }
 }
